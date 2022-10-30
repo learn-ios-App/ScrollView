ed21657776ee30cf8bad258d10156de9292cb9f4
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2022-10-31 01:27:29 +0900

    Initial Commit

---
+++ ScrollView/ScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewApp.swift
+//  ScrollView
+//
+//  Created by 渡邊魁優 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
